dcf3d38cf567203a7ea55773a7c6596a210773cf
osaavedrag <osaavedrag@unal.edu.co>
2020-02-12 14:53:20 -0500

    codigo implementado

--- hdl/src/buffer_ram_dp.v
+++ hdl/src/buffer_ram_dp.v
@@ -17,7 +17,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module buffer_ram_dp#( 
-	parameter AW = 15, // Cantidad de bits  de la dirección 
+	parameter AW = 15, // Cantidad de bits  de la direccin 
 	parameter DW = 8, // cantidad de Bits de los datos 
 	parameter   imageFILE= "src/image.men")
 	(  
@@ -31,7 +31,7 @@ module buffer_ram_dp#(
 	output reg [DW-1: 0] data_out
 	);
 
-// Calcular el número de posiciones totales de memoria 
+// Calcular el nmero de posiciones totales de memoria 
 localparam NPOS = 2 ** AW; // Memoria
 
  reg [DW-1: 0] ram [0: NPOS-1]; 
@@ -39,8 +39,10 @@ localparam NPOS = 2 ** AW; // Memoria
 
 //	 escritura  de la memoria port 1 
 always @(posedge clk_w) begin 
-       if (regwrite == 1) 
-             ram[addr_in] <= data_in;
+       if (regwrite == 1) begin
+			if(addr_in<=19200) ram[addr_in] <= data_in;
+			else ram[addr_in] <= 8'b00000000;
+			end
 end
 
 //	 Lectura  de la memoria port 2 
--- hdl/src/cam_read.v
+++ hdl/src/cam_read.v
@@ -19,18 +19,93 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module cam_read #(
-		parameter AW = 15 // Cantidad de bits  de la dirección 
+		parameter AW = 15 // Cantidad de bits  de la direcciÃ³n 
 		)(
 		input pclk,
 		input rst,
 		input vsync,
 		input href,
 		input [7:0] px_data,
+		input btnP,
 
-		output [AW-1:0] mem_px_addr,
-		output [7:0]  mem_px_data,
-		output px_wr
+		output reg [AW-1:0] mem_px_addr = 0,
+		output reg [7:0]  mem_px_data = 0,
+		output reg px_wr = 0,
+		output reg [7:0] Row,
+		output reg [7:0] Pixel
    );
+	
+reg [0:0] vsync_old = 1;
+reg [2:0] position = 0;
+reg pic = 0;
+reg [7:0] crow = 0;
+reg [7:0] cpixel = 0;
+
+always @ (posedge btnP) begin
+	
+	pic = pic + 1;
+	
+end
+
+always @ (posedge vsync) begin
+	
+	Row = crow;
+	Pixel = cpixel;
+	
+end
+
+always @ (posedge pclk) begin
+
+	case(position)
+			0: begin 
+					if (!pic) begin
+						mem_px_addr = 15'b111111111111111;
+						crow = 0;
+						if (!vsync && vsync_old) position = 1;
+					end
+				end
+			1: begin 
+					if (vsync && !vsync_old) position = 0;
+					if (href) begin
+						crow = crow + 1;
+						cpixel = 0;
+						px_wr <= 0;
+						mem_px_data[7] = px_data[7];
+						mem_px_data[6] = px_data[6];
+						mem_px_data[5] = px_data[5];
+						mem_px_data[4] = px_data[2];
+						mem_px_data[3] = px_data[1];
+						mem_px_data[2] = px_data[0];
+						position = 3;
+					end
+				end
+			2: begin 
+					px_wr <= 0;
+					cpixel = cpixel + 1;
+					mem_px_addr = mem_px_addr+1;
+					mem_px_data[7] = px_data[7];
+					mem_px_data[6] = px_data[6];
+					mem_px_data[5] = px_data[5];
+					mem_px_data[4] = px_data[2];
+					mem_px_data[3] = px_data[1];
+					mem_px_data[2] = px_data[0];
+					position = 3;
+				end
+			3: begin 
+					mem_px_data[1] = px_data[4];
+					mem_px_data[0] = px_data[3];
+					px_wr <= 1;
+					if (href)	position = 2;
+					else			position = 1;
+				end
+			default: begin
+							px_wr <= 0;
+						end
+		endcase
+		vsync_old = vsync;
+end
+
+endmodule
 	
 
 /********************************************************************************
@@ -42,4 +117,3 @@ debe tener en cuenta el nombre de las entradas  y salidad propuestas
 
 ********************************************************************************/
 
-endmodule
